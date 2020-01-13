8e696d26c75a3d8583d9e874c6dbdd2890b43c0e
Danilo Caetano Matias dos Santos <danilocaetano.santos@externe.bnpparibas.com>
2020-01-13 14:09:27 +0000

    Removing unnecessary dependencies.

--- src/main/java/com/newlight77/bootstrap/exception/NotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.newlight77.bootstrap.exception;
-
-public class NotAllowedException extends RuntimeException {
-    public NotAllowedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/pt/tabajara_airflights/bootstrap/TabajaraAirflightApp.java
+++ src/main/java/pt/tabajara_airflights/bootstrap/TabajaraAirflightApp.java
@@ -1,13 +1,13 @@
-package com.newlight77.bootstrap;
+package pt.tabajara_airflights.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TabajaraAirflightApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TabajaraAirflightApp.class, args);
 	}
 
 }
